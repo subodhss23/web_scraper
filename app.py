9019c52e05fbd5b5d330beba5d0ca26469f8d895
Subodh Sharma <subodhss2323@gmail.com>
2018-04-26 11:35:59 -0700

    Let's do this

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-print("hello World")
+__author__ = 'subodh'
+
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("http://www.johnlewis.com/store/john-lewis-wade-office-chair-black/p447855")
+soup = BeautifulSoup(r.content, "html.parser")
+element = soup.find("span", {"itemprop": "price", "class": "now-price"})
+string_value = element.text.strip() #"£115.00"
+
+price = float(string_value[1:])
+
+if price < 100:
+   print("Buy the chair!")
+elif price == 100:
+   print("Buy at your own risk!")
+else:
+   print("Definitely don't buy.")
+
+# <span itemprop="price" class="now-price"> £115.00 </span>
